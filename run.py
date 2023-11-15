c05c6aa19dc358ebc860433a430dbda69f245ad7
Aleksandro Candido <aleksandrocandido@Aleksandros-MacBook-Pro.local>
2023-11-15 17:41:47 -0300

    Add methods documentation

--- run.py
+++ run.py
@@ -23,7 +23,6 @@ Customer Class
 This is the main class which sets the attributes and methods used to manipulated customer's database
 """
 class Customer:
-    
     """
     __init__
     constructor used to create a new instance of the the Customer Class
@@ -38,14 +37,15 @@ class Customer:
         self.country = country
 
 
-#methods to be created:
+#methods to be developed:
 # remove Customer
-# update Customer
-# list single Customer
-# list all customers
 
 def menu():
-    
+    """
+    The meny method will display options to the user.
+    Each option will call the respective method to perform an action in the customers database.
+    The user should type an available option based on the numbers presented along with their actions.
+    """
     print(
         '''
         ## Options Menu ##\n
@@ -82,6 +82,13 @@ def menu():
         menu()
     
 def newCustomer():
+    """
+    The newCustomer() method is used to create a new instance of the Customer Class as well as send
+    new customer's detais to the Google Sheet using gspread.
+    The user will be requested a series of input fields in order to create the new customer.
+    The method will then call the constructor of the Customer Class passing the inputed details as parameters
+    and also use the apend_row method of worksheets to add a new line (new customer) to the Google Sheet
+    """
     print('Enter new customer details: \n')
     name = input('Name: \n')
     surname = input('Surname: \n')
@@ -101,6 +108,11 @@ def newCustomer():
     menu()
 
 def listSingleCustomer():
+    """
+    The listSingleCustomer() method will create an object retrieving all details from customers available in Google Sheets
+    These details will be inserted into a Dictionary that will then allow for search by key (which is defined as email).
+    The user will be requested the key/email as input and the system prints the details of the customer based on the provided key/email.
+    """
     details = SHEET.worksheet('details').get_values()
     customers = {}
     for index, values in enumerate(details):
@@ -126,6 +138,10 @@ def listSingleCustomer():
         menu()
 
 def listAllCustomers():
+    """
+    The listAllCustomers() method calls the get_values() Google Sheet method to retrieve all details available in the Google Sheet.
+    These details are inserted into a Dicitionary and printed as output for the user's visilibity.
+    """
     details = SHEET.worksheet('details').get_values()
     customers = {}
     for index, values in enumerate(details):
@@ -146,10 +162,17 @@ def listAllCustomers():
     print('End of the list\n')
     menu()
 
-def deleteCustomer():
-    print("delete")
-
 def updateCustomer():
+    """
+    The updateCustomer() method retrieves all customers information from Google Sheet using the get_values() method and inserting the values into a
+    Dictionary.
+    The user will be presented with an options menu where they can choose which data to be updated in the customer's database. The user will also provide
+    the key/email of the customer to be updated.
+    Each option in the menu will bring the user to a different condition in the code, within the condition the new value will be requested/inputed from the user.
+    The new value is then passed to the Google Sheet with worksheet.update_cell() method and the local Dictionary is also updated to show new details to the user.
+    Because email is used as key for the Dictionary, whenever the user chooses to update the email field, the current customer is replaced by a new customer
+    in the Dictionary as there's no option to update the key value.
+    """
     details = SHEET.worksheet('details').get_values()
     customers = {}
     counter = 2
@@ -305,5 +328,11 @@ def updateCustomer():
         print('Something went wrong :(')
         print('')
         menu()
+
+def deleteCustomer():
+    """
+    Still studying a way to delete a row in Google Sheets, for the moment thinking to use deleteDimension.
+    """
+    print("delete")
 
 menu()
